97f2a90e734fafcad4d6015d377b0bc000b5119e
Bebe Jane <bjornthief@gmail.com>
2025-01-08 18:24:16 +0100

    sound player

--- app/sound-exercises/index.tsx
+++ app/sound-exercises/index.tsx
@@ -1,9 +1,11 @@
-import { Paragraph, View, Loader, TextInput, Header } from "@/components/ui";
+import { Paragraph, ScrollView, Loader, TextInput, Header } from "@/components/ui";
 import { useQuery } from "@/lib/client";
 import { SoundExerciseDocument } from "@/graphql";
 import React from "react";
 import AudioPlayer from "@/components/AudioPlayer";
 import YoutubePlayer from "@/components/YoutubePlayer";
+import Theme from "../../styles/theme";
+import { View } from "react-native";
 
 export default function SoundExercises() {
 	const [data, error, loading] = useQuery<SoundExerciseQuery>(SoundExerciseDocument);
@@ -13,15 +15,18 @@ export default function SoundExercises() {
 	const { sovSoundExercise } = data;
 
 	return (
-		<View>
+		<ScrollView>
 			<Paragraph>{sovSoundExercise?.intro}</Paragraph>
 			{sovSoundExercise?.exercises.map(({ title, file, youtube }, i) => (
-				<React.Fragment key={i}>
+				<View
+					key={i}
+					style={{ marginBottom: Theme.margin }}
+				>
 					<Header size='small'>{title}</Header>
 					{file && <AudioPlayer src={file.url} />}
 					{youtube && <YoutubePlayer src={youtube.url} />}
-				</React.Fragment>
+				</View>
 			))}
-		</View>
+		</ScrollView>
 	);
 }
--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,33 +1,148 @@
-import { Audio } from "expo-av";
-import { Button, View } from "react-native";
+import { AVPlaybackStatus, AVPlaybackStatusSuccess, Audio } from "expo-av";
+import { View, Text } from "react-native";
+import Slider from "@react-native-community/slider";
+import React, { useEffect, useRef } from "react";
+import { Platform } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Theme from "../styles/theme";
 
 export default function AudioPlayer({ src }: { src: string }) {
-	//const player = useAudioPlayer(src);
+	const [status, setStatus] = React.useState<AVPlaybackStatusSuccess | null>(null);
+	const [error, setError] = React.useState<string | null>(null);
+	const soundRef = useRef<any>(null);
+
+	useEffect(() => {
+		const loadAudio = async () => {
+			setError(null);
+			soundRef.current = null;
+
+			try {
+				const { sound, status } = await Audio.Sound.createAsync(
+					{ uri: src },
+					{ isLooping: false, shouldPlay: false }
+				);
+
+				soundRef.current = sound;
+			} catch (e) {
+				setError((e as Error).message);
+			}
+			soundRef.current.setOnPlaybackStatusUpdate(function (status: AVPlaybackStatusSuccess) {
+				setStatus(status);
+			});
+		};
+
+		loadAudio();
+	}, []);
 
 	return (
-		<View>
-			<Button
-				title={"Play"}
-				onPress={() => loadAndPlayAudio({ uri: src })}
+		<AudioPlayerView
+			active={true}
+			playable={true}
+			loading={status?.isLoaded ? false : true}
+			isPlaying={status?.isPlaying ?? false}
+			playAudio={() => soundRef.current?.playAsync()}
+			pauseAudio={() => soundRef.current?.pauseAsync()}
+			totalDuration={status?.durationMillis ?? 0}
+			seekAudio={(value: number) => soundRef.current?.setPositionAsync(value)}
+			duration={status?.positionMillis ?? 0}
+		/>
+	);
+}
+
+type AudioPlayerViewProps = {
+	active: boolean; // is player active
+	playable: boolean; // whether we can play the specific audio or not.
+	loading: boolean; // is audio loading inside create async
+	isPlaying: boolean; // is current audio playing
+	playAudio: () => void; // callback function to play the audio.
+	pauseAudio: () => void; // callback function to pause the audio
+	totalDuration: number; // total time duration of the playable audio.
+	seekAudio: (value: number) => void; // value to jump on value for the audio (in milliseconds)
+	duration: number; // current playing duration value of audio player.
+};
+
+export const AudioPlayerView = ({
+	active,
+	playable,
+	loading,
+	isPlaying,
+	playAudio,
+	pauseAudio,
+	totalDuration,
+	seekAudio,
+	duration,
+}: AudioPlayerViewProps) => {
+	const handleIconClick = (e: any) => {
+		if (Platform.OS === "web") {
+			e?.preventDefault();
+			e?.stopPropagation();
+		}
+
+		if (!isPlaying) {
+			playAudio();
+		} else {
+			pauseAudio();
+		}
+	};
+
+	function audioDuration(currentDuration: number) {
+		if (totalDuration == 0) {
+			return "00:00";
+		}
+		const minutes = Math.floor((totalDuration - currentDuration) / 60000);
+		const seconds = parseInt((((totalDuration - currentDuration) % 60000) / 1000).toFixed(0));
+		return `-${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+	}
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				flexDirection: "row",
+				alignItems: "center",
+				width: "100%",
+				height: 40,
+				backgroundColor: Theme.color.grey,
+			}}
+		>
+			{!playable ? (
+				<View>
+					<Text>Not playable...</Text>
+				</View>
+			) : null}
+			{loading ? (
+				<View>
+					<Text>Loading...</Text>
+				</View>
+			) : (
+				<Ionicons
+					style={{ flexGrow: 1, flexShrink: 1, marginLeft: 10 }}
+					name={!isPlaying ? "play" : "pause"}
+					size={28}
+					color={Theme.color.black}
+					onPress={(e) => handleIconClick(e)}
+				/>
+			)}
+			<Slider
+				style={{
+					flexGrow: 6,
+					flexShrink: 6,
+					height: 30,
+					marginLeft: 10,
+					marginRight: 10,
+				}}
+				value={duration}
+				minimumValue={0}
+				maximumValue={totalDuration == 0 ? 100 : totalDuration}
+				step={1}
+				minimumTrackTintColor='#000000'
+				maximumTrackTintColor='#FFFFFF'
+				accessibilityLabel='Audio Player'
+				onValueChange={(step: number) => seekAudio(step)}
 			/>
+			<Text style={{ color: Theme.color.black, flexGrow: 1, flexShrink: 1, marginRight: 10 }}>
+				{audioDuration(duration)}
+			</Text>
 		</View>
 	);
-}
-
-async function loadAndPlayAudio({ uri }: { uri: string }) {
-	const { sound, status } = await Audio.Sound.createAsync(
-		{ uri },
-		{ isLooping: false, shouldPlay: false }
-	);
-	console.log(uri);
-
-	if (status.isLoaded) {
-		console.log("play");
-		await sound.setVolumeAsync(1);
-		await sound.playAsync();
-		console.log("done");
-	}
-	sound.setOnPlaybackStatusUpdate(function (status) {
-		console.log(status);
-	});
-}
+};
