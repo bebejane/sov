351397e0f9398f437a7d2451258d4469ffa053f1
Bebe Jane <bjornthief@gmail.com>
2025-01-13 09:55:59 +0100

    markdown style

--- components/ui/Paragraph.tsx
+++ components/ui/Paragraph.tsx
@@ -2,14 +2,18 @@ import { StyleSheet } from "react-native";
 import Markdown from "react-native-markdown-display";
 import Theme from "@/styles/theme";
 
-export const Paragraph = ({ children, style }: { children: any; style?: any }) => {
-	return <Markdown style={{ ...s.text, ...style }}>{children}</Markdown>;
+export const Paragraph = ({ children }: { children: any }) => {
+	return <Markdown style={s}>{children}</Markdown>;
 };
 
+// Kolla här för att styla olika element
+// https://github.com/iamacup/react-native-markdown-display/blob/master/src/lib/styles.js
+
 const s = StyleSheet.create({
-	text: {
+	body: {
+		color: "red",
 		fontSize: Theme.fontSize.default,
 		marginBottom: Theme.margin,
-		backgroundColor: '#002333',
+		backgroundColor: "#002333",
 	},
 });
