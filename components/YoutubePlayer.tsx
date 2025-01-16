b40cbad375c6403835087a740aaef09f760d556e
Bebe Jane <bjornthief@gmail.com>
2025-01-16 11:32:33 +0100

    fixxx

--- components/YoutubePlayer.tsx
+++ components/YoutubePlayer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { View } from "react-native";
 import YTPlayer from "react-native-youtube-iframe";
 
 export default function YoutubePlayer({ src }: { src: string }) {
+	if (!src) return null;
 	const youtubeId = new URL(src).searchParams.get("v");
 	if (!youtubeId) return null;
 
 	return (
-		<YTPlayer
-			height={200}
-			paddingTop={20}
-			videoId={youtubeId}
-		/>
+		<View style={{ marginBottom: 20 }}>
+			<YTPlayer
+				height={200}
+				videoId={youtubeId}
+			/>
+		</View>
 	);
 }
