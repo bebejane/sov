618affd9ba9aceb973b57957f9e8db308c78ba56
Bebe Jane <bjornthief@gmail.com>
2025-01-20 15:18:27 +0100

    image dimensions

--- components/ui/Image.tsx
+++ components/ui/Image.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Image as ImageElement } from "react-native";
+import { StyleSheet, Image as ImageElement, View } from "react-native";
+
 import Theme from "@/styles/theme";
 
 type Props = {
@@ -8,21 +9,32 @@ type Props = {
 
 export const Image = ({ src, style }: Props) => {
 	if (!src) return null;
+	const width = Theme.screenWidth - Theme.padding * 2;
+
 	return (
-		<ImageElement
-			style={[s.image, style]}
-			width={Theme.screenWidth - Theme.padding * 2}
-			height={200}
-			source={{ uri: `${src}?w=400` }}
-		/>
+		<View style={s.container}>
+			<ImageElement
+				style={[s.image, style]}
+				width={width}
+				resizeMode='contain'
+				source={{ uri: `${src}?w=1000` }}
+			/>
+		</View>
 	);
 };
 
 const s = StyleSheet.create({
+	container: {
+		flex: 1,
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "center",
+	},
 	image: {
 		objectFit: "contain",
 		marginLeft: -3,
 		marginRight: -3,
 		marginBottom: Theme.margin,
+		aspectRatio: 1,
 	},
 });
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { format } from 'date-fns'
+import { sv } from 'date-fns/locale'
 
 export const formatDate = (str: string, short: boolean = false) => {
-  return format(new Date(str), `LLL Do${!short ? ` HH:mm:ss` : ''}`)
+  return format(new Date(str), `LLL Do${!short ? ` HH:mm:ss` : ''}`, { locale: sv })
 }
